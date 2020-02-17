10d8ac7184220d8e27ff796e564082fb2304f66d
bruno <bruno@BRUNO-PC>
2020-02-17 12:44:51 -0500

    another useless commit still havent figured this out

--- src/SearchBarComponent.tsx
+++ src/SearchBarComponent.tsx
@@ -1,25 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-const SearchBarComponent = () => {
-    const[count, setCount] = useState(0);
-    const handleIncremet = () => setCount(currentCount => currentCount + 1);
-    const handDecrement = () => setCount(currentCount => currentCount - 1);
-    useEffect(() => setCount(currentCount => currentCount + 1), []);
-
-
+const SearchBarComponent = (props: any) => {
+  const [name, setName] = useState("");
+  
+  const handleSubmit = (evt: { preventDefault: () => void; }) => {
+      evt.preventDefault();
+      alert(`Submitting Name ${name}`)
+  }
     return (
-        <div className="SearchComponent">
-        <h1>{count}</h1>
-        <button type="button" onClick={handleIncremet}>
-        Increment
-      </button>
-      <button type="button" onClick={handDecrement}>
-        Decrement
-      </button>
+      <form onSubmit={handleSubmit}>
+      <label>
+        Frirst Name:
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+      //   <div className="SearchComponent">
+      //   <h1>{count}</h1>
+      //   <button type="button" onClick={handleIncremet}>
+      //   Increment
+      // </button>
+      // <button type="button" onClick={handDecrement}>
+      //   Decrement
+      // </button>
 
-      <input type="text"></input>
-      </div>
+      // <input type="text"></input>
+      // </div>
     )
 }
 
