d4db33ce13b27291c828f4fd95037d84d31e4c71
bruno <bruno@BRUNO-PC>
2020-02-29 20:48:36 -0500

    limit load to latest 20 matches

--- src/MatchStats.tsx
+++ src/MatchStats.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './MatchStats.css';
 import ToggleTray from './ToggleTray';
+import {ritoAPI} from './appsettings.json';
 
-// async function fetchData(name: string) {
-//     const proxyUrl = "https://polar-cove-15690.herokuapp.com/";
-//     const response = await fetch(proxyUrl + `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURI(name)}?api_key=${ritoAPI}`, {
-//             method: 'GET',
-//             headers: { 'Content-Type': 'application/json' },
-//             cache: 'no-cache'
-//         });
-//     return await response.json();
-// }
+async function fetchData(gameId: number) {
+    const proxyUrl = "https://polar-cove-15690.herokuapp.com/";
+    const response = await fetch(proxyUrl + `https://na1.api.riotgames.com/lol/match/v4/matches/${encodeURI(gameId.toString())}?api_key=${ritoAPI}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            cache: 'no-cache'
+        });
+    return await response.json();
+}
 
 export interface MatchinfoProps{
     platformId: string,
@@ -28,12 +29,14 @@ export interface MatchStatsProps{
 }
 
 const MatchStats : React.FC<MatchStatsProps> = props =>{
-    // [data,setData] = useState<any | undefined>(undefined);
-    // const [error, setError] = useState<any>(undefined);
+    const [data,setData] = useState<any | undefined>(undefined);
+    const [error, setError] = useState<any>(undefined);
 
-    // useEffect(() =>{
-    //     fetchData()
-    // })
+    useEffect(() => {
+        fetchData(props.match.gameId)
+        .then(data => setData(data))
+        .catch(error => setError(error));
+    },[props.match.gameId]);
 
     return <div className='MatchStats'>
         <ToggleTray title={ props.match.timestamp.toString() }>
